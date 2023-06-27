872eff775f1961f9ff5972ce5e0196fcba3addc7
Daniel <McsColosPlay@gmail.com>
2023-06-27 17:55:39 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+
+export default function App() {
+    return <h1 className="bg-slate-400 text-2xl">Hellso</h1>;
+}
---
+++ src/feature.js
@@ -0,0 +1,10 @@
+console.log("Hello from feature");
+
+export const outputName = (name) => {
+    console.log(`Hello ${name}`)
+}
+
+export const inputName = (name) => {
+    console.log(`Hello ${name}`)
+}
+
